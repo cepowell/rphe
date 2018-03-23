cf9e34f68112b3a5bc8dd750e1d82092b446863b
Cait Powell <cait.powell.92@gmail.com>
2018-03-23 02:12:19 -0700

    finishing spanish translation

---
+++ public/js/data/gallery.js
@@ -0,0 +1,10 @@
+var english_gallery = {
+  galleryText: 'Roatán Peer Health Exchange volunteers and peer health ambassadors work to provide classroom lessons, community lessons, health fairs, and community events in Roatán. Any and all donations to our organization contribute to these efforts!',
+};
+
+var spanish_gallery = {
+  galleryText: 'Los voluntarios del Roatán Peer Health Exchange y los embajadores de la salud de pares trabajan para brindar lecciones en el aula, lecciones comunitarias, ferias de salud y eventos comunitarios en Roatán. ¡Todas y cada una de las donaciones a nuestra organización contribuyen a estos esfuerzos!',
+};
+
+exports.english_gallery = english_gallery;
+exports.spanish_gallery = spanish_gallery;
---
+++ public/js/data/stories.js
@@ -0,0 +1,40 @@
+var english_stories = {
+  storiesHeader: 'Our Stories',
+  storiesSubtext: 'Read the incredible stories of the individuals involved with Roatàn Peer Health Exchange.',
+  bios: [
+    { name: 'Frida Kahlo',
+      summary: 'Painter',
+      bio: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+      picture: 'img/team/trudy.jpeg',
+      index: 1,
+    },
+    { name: 'Sonia Sotomayor',
+      summary: 'Supreme Court Justice',
+      bio: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+      picture: 'img/team/trudy.jpeg',
+      index: 2,
+    },
+  ],
+};
+
+var spanish_stories = {
+  storiesHeader: 'Our Stories',
+  storiesSubtext: 'Read the incredible stories of the individuals involved with Roatàn Peer Health Exchange.',
+  bios: [
+    { name: 'Frida Kahlo',
+      summary: 'Painter',
+      bio: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+      picture: 'img/team/trudy.jpeg',
+      index: 1,
+    },
+    { name: 'Sonia Sotomayor',
+      summary: 'Supreme Court Justice',
+      bio: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+      picture: 'img/team/trudy.jpeg',
+      index: 2,
+    },
+  ],
+};
+
+exports.english_stories = english_stories;
+exports.spanish_stories = spanish_stories;
